a91ced2685a4e2f38bd77ad4cd1533c26addcac5
podulator <github-account@podulator.com>
2019-10-20 23:00:05 +0200

    added rg350 config

--- make_opk.sh
+++ make_opk.sh
@@ -11,18 +11,19 @@ Name=Quake3
 Comment=Quake3
 Exec=ioquake3.mips
 Terminal=false
-Type=Games
+Type=Game
 StartupNotify=true
-Icon=generic
+Icon=quake
 Categories=games;
 EOF
 
 # create opk
 FLIST="default.gcw0.desktop"
-FLIST="${FLIST} generic.png"
+FLIST="${FLIST} quake.png"
 FLIST="${FLIST} build/release-gcw0-mips/ioquake3.mips"
 FLIST="${FLIST} build/release-gcw0-mips/ioq3ded.mips"
 FLIST="${FLIST} build/release-gcw0-mips/renderer_opengl1_mips.so"
+FLIST="${FLIST} baseq3"
 
 rm -f ${OPK_NAME}
 mksquashfs ${FLIST} ${OPK_NAME} -all-root -no-xattrs -noappend -no-exports
